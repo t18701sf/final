a2b39d174976208bf5746e6179bba961abbb4b02
t18701sf <45375702+t18701sf@users.noreply.github.com>
2019-01-11 13:56:06 +0900

    Update final.shogo.js

---
+++ final.shogo.js
@@ -0,0 +1,13 @@
+function timeout_callback(){
+         alert("早く始めてください！");
+      }
+      setTimeout(timeout_callback,5000);
+funtion kisotishiki(){
+   var names ;
+              names = prompt("名前を入力してください")
+              if(names == null){
+                alert("どなたですか？")
+                }
+              eles{
+                alert(name + "〜さんか〜、良い名前だね！！")
+       
