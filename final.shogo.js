f2b1ada3aba58768354129af8d50ee13b68074fc
t18701sf <45375702+t18701sf@users.noreply.github.com>
2019-01-11 14:45:08 +0900

    Update final.shogo.js

--- final.shogo.js
+++ final.shogo.js
@@ -10,4 +10,36 @@ funtion kisotishiki(){
                 }
               eles{
                 alert(name + "〜さんか〜、良い名前だね！！")
-       
+                alert("診断を開始します")
+                location.href="final1.html";
+                }
+      }   
+
+function a(){
+         location.href="final2.html";
+         }
+function b(){
+         location.href="final3.html";
+         }
+function c(){
+         location.href="final4.html";
+         }
+function d(){
+         location.href="final5.html";
+         }
+
+function goban(){
+    var x =document.getElementsByName('number1').value;
+    var y =document.getElementsByName('number2').value;
+    var z =document.getElementsByName('number3').value;
+    var q =document.getElementsByName('number4').value;
+    var score;
+    if(score =4){
+         alert("よく知っていますね！これなら安心です");
+         }else if(score > 2 || score < 3){
+         alert("いい感じです。");
+      }
+         else{
+          alert("知識が足りないですよ！");
+      }
+         
